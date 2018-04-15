912587895a6899fb0a296829cc85e5371bc49648
Luther Thompson <lutheroto@gmail.com>
2018-04-15 05:54:26 -0400

    Install flag library
    Also use install instead of cp.

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-cp --verbose --preserve --update shuffle /usr/local/bin
+install --verbose --preserve-timestamps -D --target-directory=/usr/share/guile/site/2.2 flag.scm
+install --verbose --preserve-timestamps -D --target-directory=/usr/local/bin shuffle
